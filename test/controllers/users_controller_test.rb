9f53d7fe12a57b8f62c4f51939cb56b24d807250
Cuslar <cuslar@miuandes.cl>
2019-05-25 19:03:28 -0400

    avance nuevo

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: {  } }
+      post users_url, params: { user: { address: @user.address, email: @user.email, last_name: @user.last_name, name: @user.name, password: @user.password, phone: @user.phone } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: {  } }
+    patch user_url(@user), params: { user: { address: @user.address, email: @user.email, last_name: @user.last_name, name: @user.name, password: @user.password, phone: @user.phone } }
     assert_redirected_to user_url(@user)
   end
 
